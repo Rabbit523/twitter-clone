a606037e8631770a28927d3915ca442e5761827d
Rabbit523 <wisdom.gemini@outlook.com>
2023-06-28 19:03:57 -0500

    feat: add tweet with mockup next api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,35 @@
 import { type NextPage } from "next";
+import { InfiniteTweetList } from "y/components/InfiniteTweetList";
 import { NewTweetForm } from "y/components/NewTweetForm";
+import { api } from "y/utils/api";
 
 const Home: NextPage = () => {
-  return <>
-    <header className="sticky top-0 z-10 border-b bg-white pt-2">
-      <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
-    </header>
-    <NewTweetForm />
-  </>;
+  return (
+    <>
+      <header className="sticky top-0 z-10 border-b bg-white pt-2">
+        <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
+      </header>
+      <NewTweetForm />
+      <RecentTweets />
+    </>
+  );
+};
+
+function RecentTweets() {
+  const tweets = api.tweet.infiniteFeed.useInfiniteQuery(
+    {},
+    { getNextPageParam: (lastPage) => lastPage.nextCursor }
+  );
+
+  return (
+    <InfiniteTweetList
+      tweets={tweets.data?.pages.flatMap((page) => page.tweets)}
+      isError={tweets.isError}
+      isLoading={tweets.isLoading}
+      hasMore={tweets.hasNextPage}
+      fetchNewTweets={tweets.fetchNextPage}
+    />
+  );
 }
 
 export default Home;
